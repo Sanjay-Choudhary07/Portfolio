ccc06db88e082c274ed5b1efa2b6fbd0cba8be1e
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-05-27 23:45:24 +0530

    Added new feature

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from './components/ThemeContext';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
